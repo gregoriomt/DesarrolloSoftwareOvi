faa620eaf1b9ce6d49b25ffce400dde880691f74
gregoriomt <42759363+gregoriomt@users.noreply.github.com>
2018-10-27 17:17:32 -0500

    css Accordion
    Se agregan estilos para añadir un accordion con css y javascript

--- scripts/general.js
+++ scripts/general.js
@@ -1,12 +1,29 @@
 var ObjControlador = {
 	onReady: function () {
-		ObjControlador.FechaActual();
+        ObjControlador.FechaActual();
+        ObjControlador.Accordion();
     },
     FechaActual: function () {
     	const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const d = new Date();
         var fechaCompleta = d.toLocaleDateString("es-ES", options);
     	$("#fechaActual").text(fechaCompleta);
+    },
+    Accordion: function(){
+        var accordions = document.getElementsByClassName('accordion');
+
+        for (var i = 0; i < accordions.length; i++){
+            accordions[i].onclick = function () {
+                this.classList.toggle('is-open');
+                var content = this.nextElementSibling;
+
+                if(content.style.maxHeight){
+                    content.style.maxHeight = null;
+                } else {
+                    content.style.maxHeight = content.scrollHeight + 'px';
+                }
+            }
+        }
     }
 }
 
